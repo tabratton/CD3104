fe8fe0d8fb78cb0faa840ad88a8a5b3975c51d88
Tyler Bratton <tylerbratton96@gmail.com>
2016-09-20 11:27:35 -0400

    no

--- CDA3104/AddVariables.asm
+++ CDA3104/AddVariables.asm
@@ -9,14 +9,14 @@ ExitProcess proto,dwExitCode:dword
 firstval  dword 20002000h
 secondval dword 11111111h
 thirdval  dword 22222222h
-sum dword 0
+sum       dword 0
 
 .code
 main proc
-	mov	eax,firstval				
-	add	eax,secondval		
-	add eax,thirdval
-	mov sum,eax
+     mov	eax,firstval				
+     add	eax,secondval		
+     add eax,thirdval
+     mov sum,eax
 
 	invoke ExitProcess,0
 main endp
